a8670779c073c708df1edf1c277f02e237e9b731
Gaya3Ramesh <89316721+Gaya3Ramesh@users.noreply.github.com>
2021-11-18 20:19:24 +0530

    Create index.js

---
+++ Assignment-3/index.js
@@ -0,0 +1,16 @@
+const calculator = require('./calculator'); 
+
+const addition = calculator.add(15, 101);
+console.log("Addition : " +addition);
+
+const subtraction = calculator.sub(72, 30);
+console.log("Subtraction: " +subtraction);
+
+const multiplication = calculator.mul(6, 41);
+console.log("Multiplication: " +multiplication);
+
+const division = calculator.div(7, 5);
+console.log("Division: " +division);
+
+
+
